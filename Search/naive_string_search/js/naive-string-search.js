647c46d4ebd86878dc4554cbb9c88d2482a84e3d
baconYao <bacon735392@gmail.com>
2019-10-09 07:19:59 +0800

    Finishing optimimal version of js

--- Search/naive_string_search/js/naive-string-search.js
+++ Search/naive_string_search/js/naive-string-search.js
@@ -38,3 +38,23 @@ function myNavieStringSearch(ls, ss) {
 console.log(myNavieStringSearch("wowomgzomg", "omg"));          // answer: 2
 console.log(myNavieStringSearch("wowomgzomg", "owo"));          // answer: 1
 console.log(myNavieStringSearch("wowomgzomg", "kl"));           // answer: 0
+console.log(myNavieStringSearch("lorie loled", "lol"));           // answer: 1
+console.log(myNavieStringSearch("lorie loled", "lo"));           // answer: 2
+
+function navieStringSearch(long, short) {
+    let count = 0;
+    let sl = short.length;
+    for(let i = 0; i < long.length; i++) {
+        for(let j = 0; j < sl; j++) {
+            if(short[j] != long[i+j]) break;
+            if(j == sl - 1) count++;
+        }
+    }
+    return count;
+}
+
+console.log(navieStringSearch("wowomgzomg", "omg"));          // answer: 2
+console.log(navieStringSearch("wowomgzomg", "owo"));          // answer: 1
+console.log(navieStringSearch("wowomgzomg", "kl"));           // answer: 0
+console.log(navieStringSearch("lorie loled", "lol"));           // answer: 1
+console.log(navieStringSearch("lorie loled", "lo"));           // answer: 2

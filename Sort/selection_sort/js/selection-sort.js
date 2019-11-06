3e96234ee10e133a53b51d73f3ad7e96bd25c90a
peiyao <peiyaochang@qnap.com>
2019-11-06 15:39:32 +0800

    Added js version

---
+++ Sort/selection_sort/js/selection-sort.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function mySelectionSort(unSortedArr) {
+    /* 
+    * Args:
+    *      unSortedArr {array}: an array is disorder
+    * Return:
+    *      a sorted and ascending array
+    */
+    var al = unSortedArr.length;
+    for(let i = 0; i < al - 1; i++) {
+        let sv_i = i;   // the index of the smallest value and the value is smaller than unSortedArr[i]
+        let sv = unSortedArr[i];
+        for(let j = i + 1; j < al; j++) {
+            if(unSortedArr[j] < sv) {
+                sv = unSortedArr[j];
+                sv_i = j;
+            }   
+        }
+        if(sv_i != i) {         // used to judge whether swap or not
+            let temp = unSortedArr[i];
+            unSortedArr[i] = unSortedArr[sv_i];
+            unSortedArr[sv_i] = temp;
+        }
+    }
+    return unSortedArr;
+}
+
+console.log(mySelectionSort([95,24,53,46,11,66,72,1]));
+console.log(mySelectionSort([95,24,53,46,53,66,72,1,95,34,-472,9,105,-2]));
+console.log(mySelectionSort([5,1,2,3,4]));

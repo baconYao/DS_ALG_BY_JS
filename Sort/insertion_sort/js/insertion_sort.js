21987b9ac152f18906194da369f71f6728555840
peiyao <peiyaochang@qnap.com>
2019-11-19 14:06:20 +0800

    added optimal version

--- Sort/insertion_sort/js/insertion_sort.js
+++ Sort/insertion_sort/js/insertion_sort.js
@@ -24,5 +24,19 @@ function myInsertionSort(arr) {
     return arr;
 }
 
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++) {
+        let currentValue = arr[i];
+        for(var j = i - 1; j >=0 && arr[j] > currentValue; j--) {
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = currentValue;
+    }
+    return arr;
+}
+
 console.log(myInsertionSort([5, 3, 4, 1, 2]));
 console.log(myInsertionSort([50, 34, 13, 4, 10, 21, 49, 23]));
+
+console.log(insertionSort([5, 3, 4, 1, 2]));
+console.log(insertionSort([50, 34, 13, 4, 10, 21, 49, 23]));

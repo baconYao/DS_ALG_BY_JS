df00b9acca3350be927b7a2c64322f4cdc240bb1
peiyao <peiyaochang@qnap.com>
2019-11-27 09:52:03 +0800

    Fix error

--- Sort/insertion_sort/go/main.go
+++ Sort/insertion_sort/go/main.go
@@ -1,34 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func insertionSort(arr []int) []int {
-	fmt.Println(arr)
 	for i := 1; i < len(arr); i++ {
 		cv := arr[i]
-		// fmt.Println("=====")
-		// fmt.Println(cv)
-		t := i - 1
+		var t int
 		for j := i - 1; j >= 0; j-- {
 			if arr[j] <= cv {
-				t = j
-				fmt.Println("bre")
-				fmt.Println(t)
+				t = j + 1
 				break
 			}
 			arr[j+1] = arr[j]
+			t = j
 		}
-		arr[t+1] = cv
-		fmt.Println(arr)
+		arr[t] = cv
 	}
-	// fmt.Println(arr)
 	return arr
 }
 
-// Wrong. Need to fix it
 func main() {
-	// insertionSort([]int{95, 24, 53, 46, 11, 24, 72, 95})
-	insertionSort([]int{1, 3, 4, 2})
+	fmt.Println(insertionSort([]int{95, 24, 53, 46, 11, 24, 72, 95}))
+	fmt.Println(insertionSort([]int{1, 3, 4, 2}))
 }
